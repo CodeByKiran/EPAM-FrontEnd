19f0610fa9092cf032924d43acccd3b33cc31105
Sai Kiran <122221642+CodeByKiran@users.noreply.github.com>
2025-02-12 09:01:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+let a = 10
+console.log(a);
+
+document.write(a)
+document.write("<br>"+a*a+"<br>");
+document.write("<br>"+"<br>");
+
+for(let i = 0;i<5;i++)
+{
+   for(let j = i;j<5;j++)
+   {
+     document.write("*");
+   }
+   document.write("<br>");
+}
